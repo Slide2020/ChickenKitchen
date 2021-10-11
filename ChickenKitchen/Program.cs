2e8aa6a47e1d7f9d464db3b2c5f23847922b8682
Szymon Czichy <sczichy@future-processing.com>
2021-10-11 19:33:14 +0200

    Add project files.

---
+++ ChickenKitchen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChickenKitchen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
